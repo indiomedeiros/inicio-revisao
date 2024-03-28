21ca4b99826b4c5e0eb49dffbcd3ddb71f336755
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2024-03-28 20:49:43 -0300

    adiciona funcoes de soma e exibi soma.

---
+++ tarefas.py
@@ -0,0 +1,12 @@
+def somar_numeros(numA, numB):
+    soma = numA + numB * 2 
+    return soma
+
+resultado_da_soma = somar_numeros(5, 10)
+
+
+def exibir_soma_resultados(resultado):
+    print(resultado)
+
+
+exibir_soma_resultados(resultado_da_soma)
